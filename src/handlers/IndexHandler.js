df8a08379e06b99f64e15647d90bf2b1c7f43d86
Bernard <bernard@shipityo.local>
2015-10-28 16:44:57 +0200

    =>

--- src/handlers/IndexHandler.js
+++ src/handlers/IndexHandler.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var element = React.createElement;
 var Header = require('../components/sections/Header');
 var Footer = require('../components/sections/Footer');
 var Accreditations = require('../components/sections/Accreditations');
@@ -18,8 +19,23 @@ var handler = React.createClass({
 
         return (
             <body>
+                <div id="fb-root"></div>
+                <script language="javascript" type="text/javascript" src="/js/facebook.js"></script>
                 <Header {...self.props} />
                 <AccStrip {...self.props} />
+                <section className="home-section-main">
+                    <div className="home-section-main-video">
+                        <div className="home-section-main-video-inner">
+                            <iframe width="100%" height="360px" src="https://www.youtube.com/embed/tYe0z4KZ3bs" frameBorder="0" allowFullScreen></iframe>
+                        </div>
+                    </div>
+                    <div className="home-section-main-facebook">
+                        <div className="fb-page" data-href="https://www.facebook.com/saaac.co.za"
+                             data-width="280" data-hide-cover="false" data-show-facepile="false"
+                             data-show-posts="true">
+                        </div>
+                    </div>
+                </section>
                 <ProfCourseLink {...self.props} />
                 <Accreditations {...self.props} />
                 <Footer {...self.props} />
