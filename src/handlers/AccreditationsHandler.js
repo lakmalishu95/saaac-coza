253362965de4c1d1751d0765b1dfb0d5ceba5a2f
Bernard <bernard@shipityo.local>
2016-12-04 16:07:03 +0200

    Remove fasset and qcto

--- src/handlers/AccreditationsHandler.js
+++ src/handlers/AccreditationsHandler.js
@@ -12,16 +12,12 @@ var meta = {
 };
 
 var accreditations = [
-    { "title" : "FASSET", "description" : "Accreditation No: A585000158", "logoLink" : "/images/accreditations/fasset.jpg" },
     { "title" : "Institute of Certified Bookkeepers (ICB)  ", "description" : "Accreditation No: QAP/585/003", "logoLink" : "/images/accreditations/icb.jpg" },
     { "title" : "Confederation of Tourism & Hospitality (CTH) ", "description" : "Accreditation No: 1043", "logoLink" : "/images/accreditations/cth.jpg" },
     { "title" : "TestOut ", "description" : "Authorised Educational Partner", "logoLink" : "/images/accreditations/testout.jpg" },
-    { "title" : "Quality Council for Trades & Occupations (QCTO)", "description" : "Quality Council for Trades & Occupations (QCTO) Accreditation No: NATED/14/0096", "logoLink" : "/images/accreditations/qcto.jpg" },
     { "title" : "Department of Higher Education and Training ", "description" : "Exam Centre Registration Number: 0899992811", "logoLink" : "/images/accreditations/dhet.jpg" },
     { "title" : "Chartered Institute of Tourism & Hospitality (ITHSA) ", "description" : "Accreditatition No: 1043", "logoLink" : "/images/accreditations/ith.jpg" },
     { "title" : "SACAI - Registered Matric Examination Centre", "description" : "No: A-1005-14 Umalisi Provisionally Accredited", "logoLink" : "/images/accreditations/sacai.jpg" }
-
-
 ];
 
 var handler = React.createClass({
